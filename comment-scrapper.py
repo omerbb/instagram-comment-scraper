20614c4e15456bdfaf3953033d29cf7ebc3cddf3
omerbb <42299085+omerbb@users.noreply.github.com>
2020-08-25 02:46:55 +0300

    Create comment-scrapper.py
    i need to change after line 32 to make it continue scraping comments 
    it doesnt because need to append post after for loop

---
+++ comment-scrapper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+
+
+"""https://www.instagram.com/acunilicali/?hl=tr"""
+hesap = str(input("instagram hesabının linki\n-->"))
+browser = webdriver.Firefox()
+browser.get(hesap)
+sleep(7)
+
+username=browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+password=browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+
+
+username.send_keys('botiye__')
+password.send_keys('xxtrax55')
+sleep(1)
+password.send_keys(Keys.RETURN)
+sleep(7)
+try:
+    simdi_deil=browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')
+    simdi_deil.click()
+    sleep(10)
+except:
+    pass
+
+post=browser.find_elements_by_xpath('//div[@class="v1Nh3 kIKUG  _bz0w"]')
+
+
+
+for x in range(0,len(post)):
+    post[x].click()
+    sleep(5)
+
+    comment=browser.find_elements_by_class_name('C4VMK')
+    for a in range(0,len(comment)):
+        print(comment[a].text)
+    clo = browser.find_element_by_css_selector(".BI4qX > button:nth-child(1)")
+    clo.click()
+print("finised")
+
+
